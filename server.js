8276005d5036116abf0849fa72b8d5307ded2589
AndriiZlt <andreyzlot@gmail.com>
2023-09-13 16:36:03 +0300

    mongo db password

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const app = require("./backend/index");
 const mongoose = require("mongoose");
 
 // const DB_HOST =
-//   "mongodb+srv://andriiyzlt:8paZJSeAEp5N6fm1@cluster-hw3.q6poqc8.mongodb.net/db-contacts?retryWrites=true&w=majority";
+//   "mongodb+srv://Andrii:ZEab0pkfK9O8Mi8c@cluster0.x7tlqka.mongodb.net/users?retryWrites=true&w=majority";
 
 const { DB_HOST, PORT = 3001 } = process.env;
 
 mongoose.set("strictQuery", true);
 mongoose
   .connect(
-    "mongodb+srv://andriiyzlt:8paZJSeAEp5N6fm1@cluster-hw3.q6poqc8.mongodb.net/db-contacts?retryWrites=true&w=majority"
+    "mongodb+srv://Andrii:ZEab0pkfK9O8Mi8c@cluster0.x7tlqka.mongodb.net/users?retryWrites=true&w=majority"
   )
   .then(() => {
     app.listen(PORT);
